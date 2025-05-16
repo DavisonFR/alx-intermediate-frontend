609384e087d532c54743fdb989987dec400c289e
Dawit Firew <dawitfirew439@gmail.com>
2025-05-16 21:06:52 +0300

    Initial commit: Tailwind setup complete

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"], // ✅ This makes sure Tailwind scans your files
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
